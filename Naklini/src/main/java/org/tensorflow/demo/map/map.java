ee54cd2f6e6c540edb4269c40d975ddba8e475c8
sjjam <shimjm21@gmail.com>
2020-05-05 18:09:01 +0900

    map, point modify / weather attach

--- Naklini/src/main/java/org/tensorflow/demo/map/map.java
+++ Naklini/src/main/java/org/tensorflow/demo/map/map.java
@@ -432,7 +432,6 @@ public class map extends AppCompatActivity implements OnMapReadyCallback {
         }
     }
 
-    // TODO: 멀티캠퍼스로 이동
     public void setPosition(){
         LatLng myloc = new LatLng(37.5013395,127.0393345);
         // 카메라가 이동할때 애니메이션이 적용
@@ -454,6 +453,7 @@ public class map extends AppCompatActivity implements OnMapReadyCallback {
 
         //String latitude = onboardList.get(1).getLatitude(); // 위도
         //String longitude = onboardList.get(1).getLongitude(); // 경도
+
         setPosition();
         for (int i=1; i<onboardList.size(); i++){
             String latitude2 = onboardList.get(i).getLatitude();
@@ -503,6 +503,7 @@ public class map extends AppCompatActivity implements OnMapReadyCallback {
 
         //String latitude = rockList.get(1).getLatitude(); // 위도
         //String longitude = rockList.get(1).getLongitude(); // 경도
+
         setPosition();
         for (int i=1; i<rockList.size(); i++){
             String latitude2 = rockList.get(i).getLatitude();
